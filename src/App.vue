84b0daa42be500d872d881ffa1a7ff13c53be4d1
wkl007 <499657357@qq.com>
2020-09-29 21:53:06 +0800

    feat: 添加小demo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
 
 <style lang="less">
 #app {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,67 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul>
+      <li
+        v-for="(item,index) in list"
+        :key="index"
+        :class="item.active?'active':''"
+        @click="toggleList(item,index)"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+    <button @click="processObject">对象处理</button>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+interface ListProps {
+  name: string;
+  active?: boolean;
+}
+
+export default defineComponent({
+  name: 'about',
+  data () {
+    return {
+      list: [
+        {
+          name: '1'
+        },
+        {
+          name: '2'
+        },
+        {
+          name: '3'
+        },
+        {
+          name: '4'
+        }
+      ] as Array<ListProps>,
+      obj: {
+        name: '2222',
+        title: 222
+      } as ListProps
+    }
+  },
+  methods: {
+    toggleList (item: ListProps, index: number): void {
+      this.list[index].active = !this.list[index].active
+      console.log(this.list[index])
+    },
+    processObject (): void {
+      this.obj.active = !this.obj.active
+      console.log(this.obj)
+    }
+  }
+})
+</script>
+
+<style lang="less">
+.active {
+  background: red;
+}
+</style>
