04efee380a5bee63bcd865acf6a0531c2678bd12
wkl007 <499657357@qq.com>
2020-11-10 19:30:35 +0800

    chore: 添加babel-polyfill

--- vue.config.js
+++ vue.config.js
@@ -69,6 +69,42 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
+
+    // externals配置
+    const externals = {
+      // axios: 'axios'
+    }
+    config.externals(externals)
+    // cdn配置
+    const cdnUrl = 'https://cdn.jsdelivr.net/npm/'
+    const cdn = {
+      // 开发环境
+      dev: {
+        css: [],
+        js: [
+          // babel-polyfill
+          `${cdnUrl}babel-polyfill@6.26.0/dist/polyfill.js`
+        ]
+      },
+      // 生产环境
+      build: {
+        css: [],
+        js: [
+          // babel-polyfill
+          `${cdnUrl}babel-polyfill@6.26.0/dist/polyfill.min.js`
+        ]
+      }
+    }
+
+    config.plugin('html').tap(args => {
+      if (process.env.NODE_ENV === 'production') {
+        args[0].cdn = cdn.build
+      }
+      if (process.env.NODE_ENV === 'development') {
+        args[0].cdn = cdn.dev
+      }
+      return args
+    })
   },
   // css相关配置
   css: {
