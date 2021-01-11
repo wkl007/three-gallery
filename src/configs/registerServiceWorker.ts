34b1fa0706491d6aba02961f362fcd4914958f22
wkl007 <499657357@qq.com>
2021-01-11 21:39:46 +0800

    feat: SW更新提示

--- src/configs/registerServiceWorker.ts
+++ src/configs/registerServiceWorker.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
+import { notification } from 'ant-design-vue'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -21,6 +22,11 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
+      notification.info({
+        message: '提示',
+        description: '发现新内容可用，请刷新页面',
+        duration: null
+      })
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
